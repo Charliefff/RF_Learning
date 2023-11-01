06480e5c85e43b6a7cbb2f16c72a5bd0c919b9f1
Charlie <charlie88531@gnail.com>
2023-11-02 00:09:04 +0800

    Hw3

--- HW3/code/models/atari_model.py
+++ HW3/code/models/atari_model.py
@@ -4,45 +4,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Categorical
 
+
 class AtariNet(nn.Module):
     def __init__(self, num_classes=4, init_weights=True):
         super(AtariNet, self).__init__()
 
         self.cnn = nn.Sequential(nn.Conv2d(4, 32, kernel_size=8, stride=4),
-                                        nn.ReLU(True),
-                                        nn.Conv2d(32, 64, kernel_size=4, stride=2),
-                                        nn.ReLU(True),
-                                        nn.Conv2d(64, 64, kernel_size=3, stride=1),
-                                        nn.ReLU(True)
-                                        )
+                                 nn.ReLU(True),
+                                 nn.Conv2d(32, 64, kernel_size=4, stride=2),
+                                 nn.ReLU(True),
+                                 nn.Conv2d(64, 64, kernel_size=3, stride=1),
+                                 nn.ReLU(True)
+                                 )
         self.action_logits = nn.Sequential(nn.Linear(7*7*64, 512),
-                                        nn.ReLU(True),
-                                        nn.Linear(512, num_classes)
-                                        )
+                                           nn.ReLU(True),
+                                           nn.Linear(512, num_classes)
+                                           )
+        
+        # 有更改value 網路
         self.value = nn.Sequential(nn.Linear(7*7*64, 512),
-                                        nn.ReLU(True),
-                                        nn.Linear(512, 1)
-                                        )
+                                   nn.Tanh(),
+                                   nn.Linear(512, 256),
+                                   nn.Tanh(),
+                                   nn.Linear(256, 1)
+                                   )
 
         if init_weights:
             self._initialize_weights()
-
-    def forward(self, x, eval=False):
-        x = x.float() / 255.
-        x = self.cnn(x)
-        x = torch.flatten(x, start_dim=1)
-        value = self.value(x)
-        value = torch.squeeze(value)
-
-        logits = self.action_logits(x)
-        
-        dist = Categorical(logits=logits)
-        
-        ### TODO ###
-        # Finish the forward function
-        # Return action, action probability, value, entropy
-
-        return NotImplementedError
 
     def _initialize_weights(self):
         for m in self.modules():
@@ -52,6 +40,22 @@ class AtariNet(nn.Module):
             elif isinstance(m, nn.Linear):
                 nn.init.orthogonal_(m.weight, np.sqrt(2))
                 nn.init.constant_(m.bias, 0.0)
-                
 
+    def forward(self, x, eval=False):
+        x = x.float() / 255.
+        x = self.cnn(x)
+        x = torch.flatten(x, start_dim=1)
+        # actor 網路
+        logits = self.action_logits(x)
 
+        # value 網路
+        value = self.value(x)
+        value = torch.squeeze(value)
+
+        dist = Categorical(logits=logits) #得到機率分布
+
+        ### TODO ###
+        # Finish the forward function
+        # Return action, action probability, value, entropy
+
+        return NotImplementedError
--- HW3/code/ppo_agent_atari.py
+++ HW3/code/ppo_agent_atari.py
@@ -12,115 +12,132 @@ import gym
 
 
 class AtariPPOAgent(PPOBaseAgent):
-	def __init__(self, config):
-		super(AtariPPOAgent, self).__init__(config)
-		### TODO ###
-		# initialize env
-		# self.env = ???
-		
-		### TODO ###
-		# initialize test_env
-		# self.test_env = ???
+    def __init__(self, config):
+        super(AtariPPOAgent, self).__init__(config)
+        self.env = gym.make('ALE/Breakout-v5', render_mode="rgb_array")
+        self.env = gym.wrappers.AtariPreprocessing(
+            self.env, noop_max=30, frame_skip=1, screen_size=84, terminal_on_life_loss=False, grayscale_obs=True)
+        self.env = gym.wrappers.FrameStack(self.env, 4)
 
-		self.net = AtariNet(self.env.action_space.n)
-		self.net.to(self.device)
-		self.lr = config["learning_rate"]
-		self.update_count = config["update_ppo_epoch"]
-		self.optim = torch.optim.Adam(self.net.parameters(), lr=self.lr)
-		
-	def decide_agent_actions(self, observation, eval=False):
-		### TODO ###
-		# add batch dimension in observation
-		# get action, value, logp from net
-		
-		# if eval:
-		# 	with torch.no_grad():
-		# 		???, ???, ???, _ = self.net(observation, eval=True)
-		# else:
-		# 	???, ???, ???, _ = self.net(observation)
-		
-		return NotImplementedError
+        self.test_env = gym.make('ALE/Breakout-v5', render_mode="rgb_array")
+        self.test_env = gym.wrappers.AtariPreprocessing(
+            self.test_env, noop_max=30, frame_skip=1, screen_size=84, terminal_on_life_loss=False, grayscale_obs=True)
+        self.test_env = gym.wrappers.FrameStack(self.test_env, 4)
 
-	
-	def update(self):
-		loss_counter = 0.0001
-		total_surrogate_loss = 0
-		total_v_loss = 0
-		total_entropy = 0
-		total_loss = 0
+        self.net = AtariNet(self.env.action_space.n)
+        self.net.to(self.device)
+		# 交互對象
+        self.net_old = AtariNet(self.env.action_space.n)
+        self.net_old.to(self.device)
+        self.net_old.load_state_dict(self.net.state_dict())
 
-		batches = self.gae_replay_buffer.extract_batch(self.discount_factor_gamma, self.discount_factor_lambda)
-		sample_count = len(batches["action"])
-		batch_index = np.random.permutation(sample_count)
-		
-		observation_batch = {}
-		for key in batches["observation"]:
-			observation_batch[key] = batches["observation"][key][batch_index]
-		action_batch = batches["action"][batch_index]
-		return_batch = batches["return"][batch_index]
-		adv_batch = batches["adv"][batch_index]
-		v_batch = batches["value"][batch_index]
-		logp_pi_batch = batches["logp_pi"][batch_index]
+        self.lr = config["learning_rate"]
+        self.update_count = config["update_ppo_epoch"]
+        self.optim = torch.optim.Adam(self.net.parameters(), lr=self.lr)
 
-		for _ in range(self.update_count):
-			for start in range(0, sample_count, self.batch_size):
-				ob_train_batch = {}
-				for key in observation_batch:
-					ob_train_batch[key] = observation_batch[key][start:start + self.batch_size]
-				ac_train_batch = action_batch[start:start + self.batch_size]
-				return_train_batch = return_batch[start:start + self.batch_size]
-				adv_train_batch = adv_batch[start:start + self.batch_size]
-				v_train_batch = v_batch[start:start + self.batch_size]
-				logp_pi_train_batch = logp_pi_batch[start:start + self.batch_size]
+    def decide_agent_actions(self, observation, eval=False):
+        ### TODO ###
+        # add batch dimension in observation
+        # get action, value, logp from net
 
-				ob_train_batch = torch.from_numpy(ob_train_batch["observation_2d"])
-				ob_train_batch = ob_train_batch.to(self.device, dtype=torch.float32)
-				ac_train_batch = torch.from_numpy(ac_train_batch)
-				ac_train_batch = ac_train_batch.to(self.device, dtype=torch.long)
-				adv_train_batch = torch.from_numpy(adv_train_batch)
-				adv_train_batch = adv_train_batch.to(self.device, dtype=torch.float32)
-				logp_pi_train_batch = torch.from_numpy(logp_pi_train_batch)
-				logp_pi_train_batch = logp_pi_train_batch.to(self.device, dtype=torch.float32)
-				return_train_batch = torch.from_numpy(return_train_batch)
-				return_train_batch = return_train_batch.to(self.device, dtype=torch.float32)
+        # if eval:
+        # 	with torch.no_grad():
+        # 		???, ???, ???, _ = self.net(observation, eval=True)
+        # else:
+        # 	???, ???, ???, _ = self.net(observation)
 
-				### TODO ###
-				# calculate loss and update network
-				# ???, ???, ???, ??? = self.net(...)
+        return NotImplementedError
 
-				# calculate policy loss
-				# ratio = ???
-				# surrogate_loss = ???
+    def update(self):
+        loss_counter = 0.0001
+        total_surrogate_loss = 0
+        total_v_loss = 0
+        total_entropy = 0
+        total_loss = 0
 
-				# calculate value loss
-				# value_criterion = nn.MSELoss()
-				# v_loss = value_criterion(...)
-				
-				# calculate total loss
-				# loss = surrogate_loss + self.value_coefficient * v_loss - self.entropy_coefficient * entropy
+        batches = self.gae_replay_buffer.extract_batch(
+            self.discount_factor_gamma, self.discount_factor_lambda)
+        sample_count = len(batches["action"])
+        batch_index = np.random.permutation(sample_count)
 
-				# update network
-				# self.optim.zero_grad()
-				# loss.backward()
-				# nn.utils.clip_grad_norm_(self.net.parameters(), self.max_gradient_norm)
-				# self.optim.step()
+        observation_batch = {}
+        # 取數據
+        for key in batches["observation"]:
+            observation_batch[key] = batches["observation"][key][batch_index]
+        action_batch = batches["action"][batch_index]
+        return_batch = batches["return"][batch_index]
+        adv_batch = batches["adv"][batch_index]
+        v_batch = batches["value"][batch_index]
+        logp_pi_batch = batches["logp_pi"][batch_index]
 
-				# total_surrogate_loss += surrogate_loss.item()
-				# total_v_loss += v_loss.item()
-				# total_entropy += entropy.item()
-				# total_loss += loss.item()
-				# loss_counter += 1
+		# 更新
+        for _ in range(self.update_count):
+            for start in range(0, sample_count, self.batch_size):
+                ob_train_batch = {}
+                for key in observation_batch:
+                    ob_train_batch[key] = observation_batch[key][start:start + self.batch_size]
+                ac_train_batch = action_batch[start:start + self.batch_size]
+                return_train_batch = return_batch[start:start +
+                                                  self.batch_size]
+                adv_train_batch = adv_batch[start:start + self.batch_size]
+                v_train_batch = v_batch[start:start + self.batch_size]
+                logp_pi_train_batch = logp_pi_batch[start:start +
+                                                    self.batch_size] 
 
-		self.writer.add_scalar('PPO/Loss', total_loss / loss_counter, self.total_time_step)
-		self.writer.add_scalar('PPO/Surrogate Loss', total_surrogate_loss / loss_counter, self.total_time_step)
-		self.writer.add_scalar('PPO/Value Loss', total_v_loss / loss_counter, self.total_time_step)
-		self.writer.add_scalar('PPO/Entropy', total_entropy / loss_counter, self.total_time_step)
-		print(f"Loss: {total_loss / loss_counter}\
+                ob_train_batch = torch.from_numpy(
+                    ob_train_batch["observation_2d"])
+                ob_train_batch = ob_train_batch.to(
+                    self.device, dtype=torch.float32)
+                ac_train_batch = torch.from_numpy(ac_train_batch)
+                ac_train_batch = ac_train_batch.to(
+                    self.device, dtype=torch.long)
+                adv_train_batch = torch.from_numpy(adv_train_batch)
+                adv_train_batch = adv_train_batch.to(
+                    self.device, dtype=torch.float32)
+                logp_pi_train_batch = torch.from_numpy(logp_pi_train_batch)
+                logp_pi_train_batch = logp_pi_train_batch.to(
+                    self.device, dtype=torch.float32)
+                return_train_batch = torch.from_numpy(return_train_batch)
+                return_train_batch = return_train_batch.to(
+                    self.device, dtype=torch.float32)
+
+                ### TODO ###
+                # calculate loss and update network
+                # ???, ???, ???, ??? = self.net(...)
+
+                # calculate policy loss
+                # ratio = ???
+                # surrogate_loss = ???
+
+                # calculate value loss
+                # value_criterion = nn.MSELoss()
+                # v_loss = value_criterion(...)
+
+                # calculate total loss
+                # loss = surrogate_loss + self.value_coefficient * v_loss - self.entropy_coefficient * entropy
+
+                # update network
+                # self.optim.zero_grad()
+                # loss.backward()
+                # nn.utils.clip_grad_norm_(self.net.parameters(), self.max_gradient_norm)
+                # self.optim.step()
+
+                # total_surrogate_loss += surrogate_loss.item()
+                # total_v_loss += v_loss.item()
+                # total_entropy += entropy.item()
+                # total_loss += loss.item()
+                # loss_counter += 1
+		# 更新old
+        self.writer.add_scalar('PPO/Loss', total_loss /
+                               loss_counter, self.total_time_step)
+        self.writer.add_scalar(
+            'PPO/Surrogate Loss', total_surrogate_loss / loss_counter, self.total_time_step)
+        self.writer.add_scalar(
+            'PPO/Value Loss', total_v_loss / loss_counter, self.total_time_step)
+        self.writer.add_scalar(
+            'PPO/Entropy', total_entropy / loss_counter, self.total_time_step)
+        print(f"Loss: {total_loss / loss_counter}\
 			\tSurrogate Loss: {total_surrogate_loss / loss_counter}\
 			\tValue Loss: {total_v_loss / loss_counter}\
 			\tEntropy: {total_entropy / loss_counter}\
 			")
-	
-
-
-
