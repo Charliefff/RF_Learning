5f7d93c97c9c235f0f5dde819796472c7a75b5fe
Charlie <charlie88531@gnail.com>
2023-11-08 18:59:49 +0800

    HW3

---
+++ HW3/Add_noise/main.py
@@ -0,0 +1,27 @@
+from ppo_agent_atari import AtariPPOAgent
+
+if __name__ == '__main__':
+
+    config = {
+        "gpu": True,
+        "training_steps": 1e8,
+        "update_sample_count": 10000,
+        "discount_factor_gamma": 0.99,
+        "discount_factor_lambda": 0.95,
+        "clip_epsilon": 0.2,
+        "max_gradient_norm": 0.5,
+        "batch_size": 256,
+        "logdir": '/data/tzeshinchen/RF_Learning/HW3/Add_noise/log/Euduro_34193871/',
+        "update_ppo_epoch": 3,
+        "learning_rate": 2.5e-5,
+        "value_coefficient": 0.5,
+        "entropy_coefficient": 0,
+        "horizon": 128,
+        "env_id": 'ALE/Enduro-v5',
+        "eval_interval": 30,
+        "eval_episode": 3,
+    }
+    agent = AtariPPOAgent(config)
+    agent.load(
+        "/data/tzeshinchen/RF_Learning/HW3/Add_noise/log/Euduro_22403934/model_34193871_1873.pth")
+    agent.train()
---
+++ HW3/Add_noise/models/atari_model.py
@@ -0,0 +1,60 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
+
+
+class AtariNet(nn.Module):
+    def __init__(self, num_classes=4, init_weights=True):
+        super(AtariNet, self).__init__()
+
+        self.cnn = nn.Sequential(layer_init(nn.Conv2d(4, 32, kernel_size=8, stride=4)),
+                                 nn.ReLU(True),
+                                 layer_init(
+                                     nn.Conv2d(32, 64, kernel_size=4, stride=2)),
+                                 nn.ReLU(True),
+                                 layer_init(
+                                     nn.Conv2d(64, 64, kernel_size=3, stride=1)),
+                                 nn.ReLU(True),
+                                 nn.Flatten(),
+                                 layer_init(nn.Linear(7*7*64, 512)),
+                                 nn.ReLU(True)
+                                 )
+
+        # actor
+        self.action_logits = nn.Sequential(
+            layer_init(nn.Linear(512, num_classes), std=0.01)
+        )
+
+        # critic
+        self.value = nn.Sequential(
+            layer_init(nn.Linear(512, 1), std=1.0)
+        )
+
+    def forward(self, x, eval=False, new_action=None):
+
+        x = x.float() / 255.
+        x = self.cnn(x)
+        value = self.value(x)
+        value = torch.squeeze(value)
+
+        logits = self.action_logits(x)
+
+        dist = Categorical(logits=logits)
+
+        if eval:
+            action = torch.argmax(logits, dim=1)
+        else:
+            if new_action is None:
+                action = dist.sample()
+            else:
+                action = new_action
+
+        return action, value, dist.log_prob(action), dist.entropy()
